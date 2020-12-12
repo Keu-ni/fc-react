71165fdb61d70e50ddd1432df2286d8146fce0e4
Keu-ni <skcor8909@gmail.com>
2020-12-12 22:39:50 +0900

    useEffect basic

--- src/UserList.js
+++ src/UserList.js
@@ -1,8 +1,23 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
 function User(props) {
     const { user, onRemove, onToggle } = props;
     const { id, username, email, active} = user;
+
+    useEffect(() => {
+        // 컴포넌트가 마운트 될 때
+        // ex) props -> state
+        // REST API or library
+        // setTimeout ...
+        console.log('Component render!');
+        return () => {
+
+            // 언마운트 될 때
+            // clearTimeout
+            // 라이브러리 인스턴스 제거
+            console.log('Component Delete!');
+        }
+    }, []);
     return (
         <div>
             <b style={{
