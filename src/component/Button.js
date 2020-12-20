1f435be77c31bb398a03fc2beb42721fc9122cdb
Keu-ni <skcor8909@gmail.com>
2020-12-20 22:30:52 +0900

    scss

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,13 +1,24 @@
-import React from "react";
-import classNames from "classnames";
-import "./Button.scss";
+import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
 
-function Button({ children, size }) {
-  return <button className={classNames("Button", size)}>{children}</button>;
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
+  return (
+    <button
+      className={classNames('Button', size, color, {
+        outline,
+        fullWidth,
+      })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.defaultProps = {
-  size: "medium",
+  size: 'medium',
+  color: 'blue',
 };
 
 export default Button;
