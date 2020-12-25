023b9044160b9d700976cdc27e77b181974a85a1
Keu-ni <skcor8909@gmail.com>
2020-12-25 22:35:00 +0900

    RESTful API get data to useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useReducer, useRef, useMemo, useCallback } from 'react';
-import CreateUser from './CreateUser';
-import UserList from './UserList';
-import useInputs from './useInputs';
+import React, { useReducer, useRef, useMemo, useCallback } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
+import useInputs from "./useInputs";
+import Users from "./Users";
 
 const initialState = {
   // inputs: {
@@ -10,25 +11,25 @@ const initialState = {
   // },
   users: [
     {
-        id: 1,
-        username: 'hongkeun',
-        email: '123@123.com',
-        active: true,
+      id: 1,
+      username: "hongkeun",
+      email: "123@123.com",
+      active: true,
     },
     {
-        id: 2,
-        username: 'Keuni',
-        email: 'kenui@123.com',
-        active: false,
+      id: 2,
+      username: "Keuni",
+      email: "kenui@123.com",
+      active: false,
     },
     {
-        id: 3,
-        username: 'sixshopKeuni',
-        email: 'widjai@123.com',
-        active: false,
+      id: 3,
+      username: "sixshopKeuni",
+      email: "widjai@123.com",
+      active: false,
     },
-  ]
-}
+  ],
+};
 
 function reducer(state, action) {
   switch (action.type) {
@@ -40,41 +41,37 @@ function reducer(state, action) {
     //       [action.name]: action.value,
     //     }
     //   }
-    case 'CREATE_USER':
+    case "CREATE_USER":
       return {
         inputs: initialState.inputs,
         users: state.users.concat(action.user),
-      }
-    case 'TOGGLE_USER':
+      };
+    case "TOGGLE_USER":
       return {
         ...state,
-        users: state.users.map(user => 
-          user.id === action.id 
-            ? { ...user, active: !user.active }
-            : user
-          )
-      }
-    case 'REMOVE_USER': 
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
       return {
         ...state,
-        users: state.users.filter(user => 
-          user.id !== action.id
-          )
-      }
+        users: state.users.filter((user) => user.id !== action.id),
+      };
     default:
-      throw new Error('Unhandled action');
+      throw new Error("Unhandled action");
   }
 }
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [form, onChange, reset] = useInputs({
-    username: '',
-    email: '',
+    username: "",
+    email: "",
   });
-  const { username, email} = form;
+  const { username, email } = form;
   const { users } = state;
-  const nextId = useRef((initialState.users).length + 1);
+  const nextId = useRef(initialState.users.length + 1);
   // const { username, email } = state.inputs;
 
   // const onChange = useCallback(e => {
@@ -83,55 +80,55 @@ function App() {
   //     type: 'CHANGE_INPUT',
   //     name,
   //     value,
-  //   }) 
+  //   })
   // }, [])
 
-  const onCreate = useCallback(e => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email,
-      }
-    });
-    nextId.current += 1;
-    reset();
-  }, [username, email, reset]);
+  const onCreate = useCallback(
+    (e) => {
+      dispatch({
+        type: "CREATE_USER",
+        user: {
+          id: nextId.current,
+          username,
+          email,
+        },
+      });
+      nextId.current += 1;
+      reset();
+    },
+    [username, email, reset]
+  );
 
-  const onToggle = useCallback(id => {
+  const onToggle = useCallback((id) => {
     dispatch({
-      type: 'TOGGLE_USER',
-      id
+      type: "TOGGLE_USER",
+      id,
     });
   }, []);
 
-  const onRemove = useCallback(id => {
+  const onRemove = useCallback((id) => {
     dispatch({
-      type: 'REMOVE_USER',
-      id
+      type: "REMOVE_USER",
+      id,
     });
   }, []);
 
   function getActiveUserLength(users) {
-    return users.filter(user => (user.active)).length;
+    return users.filter((user) => user.active).length;
   }
 
   const count = useMemo(() => getActiveUserLength(users), [users]);
 
   return (
     <>
-      <CreateUser 
-        username={username} 
-        email={email} 
+      <Users />
+      <CreateUser
+        username={username}
+        email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList 
-        users={users}
-        onToggle={onToggle}
-        onRemove={onRemove}
-      />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>active: {count}</div>
     </>
   );
